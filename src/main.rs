b10f74f8d88b6cc8d0ae1fe3f422c8cd50c3445e
Ben Weedon <ben@weedon.email>
2023-01-06 17:01:26 -0800

    Add basic argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{self, Parser};
+
+#[derive(Parser)]
+struct Opts {
+    #[command(subcommand)]
+    subcommand: Subcommand,
+}
+
+#[derive(clap::Subcommand)]
+enum Subcommand {
+    /// Dump the UIA tree on the current desktop.
+    Dump,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opts = Opts::parse();
+    match opts.subcommand {
+        Subcommand::Dump => println!("dumping…"),
+    }
 }
